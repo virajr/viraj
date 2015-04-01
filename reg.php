e4c5ee658c8b34f6e3ec495251f6c27e9408372f
Viraj Rajankar <virajr@users.noreply.github.com>
2015-04-01 18:41:49 +0530

    Update reg.php

--- reg.php
+++ reg.php
@@ -18,7 +18,7 @@
 <body>
 <h1>Register here!</h1>
 <p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
-<form method="post" action="index.php" enctype="multipart/form-data" >
+<form method="post" action="" enctype="multipart/form-data" >
       Name  <input type="text" name="name" id="name"/></br>
       Email <input type="text" name="email" id="email"/></br>
       <input type="submit" name="submit" value="Submit" />
